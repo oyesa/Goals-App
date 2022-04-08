d36bc82bf06af766fef5580742d73f5e3a37c493
oyesa <mercyoyesa2018@gmail.com>
2022-04-08 15:27:24 +0300

    Add goal functionalities

---
+++ src/app/goal-detail/goal-detail.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+// import { EventEmitter } from 'stream';
+import { Goal } from '../goal';
+
+@Component({
+  selector: 'app-goal-detail',
+  templateUrl: './goal-detail.component.html',
+  styleUrls: ['./goal-detail.component.css']
+})
+export class GoalDetailComponent implements OnInit {
+
+  @Input() goals: Goal;
+  @Output() isComplete = new EventEmitter <boolean>();
+
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
